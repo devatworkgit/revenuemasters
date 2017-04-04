1a964b563b7a27eb0d70aad7215351b52498ad50
Ion <macaria.ion@gmail.com>
2017-04-04 17:51:32 +0300

    last changes

--- themes/custom/revenuemasters/js/header.js
+++ themes/custom/revenuemasters/js/header.js
@@ -1,14 +1,23 @@
 (function($){
+  
   Drupal.behaviors.header_behaviors = {
     attach: function(context, settings) {
+      $(document).ready(function (event) {
+        detect_dead_position();
+      });
       $(window).scroll(function (event) {
-        var scroll = $(window).scrollTop();
-        if (scroll != 0 && !$('header#navbar').hasClass('scrolled')) {
-          $('header#navbar').addClass('scrolled');
-        } else if (scroll == 0 && $('header#navbar').hasClass('scrolled')) {
-          $('header#navbar').removeClass('scrolled');
-        }
+        detect_dead_position();
       });
     }
   };
+  
+  function detect_dead_position() {
+    var scroll = $(window).scrollTop();
+    if (scroll != 0 && !$('header#navbar').hasClass('scrolled')) {
+      $('header#navbar').addClass('scrolled');
+    } else if (scroll == 0 && $('header#navbar').hasClass('scrolled')) {
+      $('header#navbar').removeClass('scrolled');
+    }
+  }
+  
 })(jQuery);
